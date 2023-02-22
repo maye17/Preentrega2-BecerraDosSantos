da2a4ee04765f9fc5fe032534dac4f1daf24761e
maye17 <mayerlin.dossantos@gmail.com>
2023-02-21 22:47:45 -0300

    agregando category:id

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 const CategoryList =()=> {
     const [articles, setArticle]= useState([]);
-    const [categoires, setCategories]= useState(allCategories);
+ 
 
     const datos = "https://fakestoreapi.com/products/";
 
@@ -21,27 +21,32 @@ const CategoryList =()=> {
         
         const allCategories = ['All',
         ...new Set(articles.map((article) => (article.category)))];
-   
+
+      
     
         const filterCategory =(category)=> {
            
             if(category==='All'){
                 setArticle(articles)
-                return (console.log(setArticle))
+                return 
             }
             const filterData =articles.filter(article =>article.category===category)
-            console.log(filterData);
             setArticle(filterData)
             } 
+
+            const [categories, setCategories]= useState(allCategories);
     
         return (
             <div>
-                {categories.filter((category) => {
+                {categories.map((category) => {
                         return (
-                 <Link to={`/category/${category.id}`}> 
-                <ButtonList categories={allCategories} filterCategory={filterCategory}>
-                </ButtonList>
-                </Link>
+                            <div>
+                                 <Link to={`/category/${category.id}`}> 
+                                    <ButtonList categories={allCategories} filterCategory={filterCategory}>
+                                    </ButtonList>
+                                </Link>
+                            </div>
+                    
                         )})}
                 <div className="card-list">
                     {articles.map((article) => {
