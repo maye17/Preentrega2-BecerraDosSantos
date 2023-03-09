fb2bebcbe0b279d6a16b24e3b6d7b569bcd06d13
maye17 <mayerlin.dossantos@gmail.com>
2023-03-08 22:03:41 -0300

    Updates

---
+++ precache-manifest.bbaa4dffee456d96d8384297d84705fc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7677132828621bbf00ca",
+    "url": "/Preentrega2-BecerraDosSantos/static/css/main.b1641fd2.chunk.css"
+  },
+  {
+    "revision": "7677132828621bbf00ca",
+    "url": "/Preentrega2-BecerraDosSantos/static/js/main.76771328.chunk.js"
+  },
+  {
+    "revision": "15c129fab2f417cd0741",
+    "url": "/Preentrega2-BecerraDosSantos/static/css/1.58c3e133.chunk.css"
+  },
+  {
+    "revision": "15c129fab2f417cd0741",
+    "url": "/Preentrega2-BecerraDosSantos/static/js/1.15c129fa.chunk.js"
+  },
+  {
+    "revision": "43cb2c6709271d4fa4fc",
+    "url": "/Preentrega2-BecerraDosSantos/static/js/runtime~main.43cb2c67.js"
+  },
+  {
+    "revision": "e8b81e26d00788da9c98a431bb4e12d7",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/maiVisage.e8b81e26.jpg"
+  },
+  {
+    "revision": "9220acf914ba6eedd9366a29a0c71b55",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/a8ccs-gcnmr.9220acf9.svg"
+  },
+  {
+    "revision": "f4e3116d44c6d5387ef93a93eacdf4e7",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/imagen1.f4e3116d.svg"
+  },
+  {
+    "revision": "a4363714ddf7c31980b88ceabba0968e",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/imagen2.a4363714.svg"
+  },
+  {
+    "revision": "875d1b33bd0d95f1b3ed9cbd4928d395",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/panoramica1.875d1b33.jpg"
+  },
+  {
+    "revision": "48b399c263a357c86a060d3d516c7cf4",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/panoramica2.48b399c2.jpg"
+  },
+  {
+    "revision": "23979b402d2a95cbbcba15477299a3bd",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/panoramica3.23979b40.jpg"
+  },
+  {
+    "revision": "f7e6db4bfb44ebcc871b229ed5f8d7c0",
+    "url": "/Preentrega2-BecerraDosSantos/static/media/1609-maquillaje.f7e6db4b.jpg"
+  },
+  {
+    "revision": "a8e43113a5da8a08902b796fd3160875",
+    "url": "/Preentrega2-BecerraDosSantos/index.html"
+  }
+];
